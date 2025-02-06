8f5a3d22d1413a8d28d978f65ac1f134bb5f45dd
aado97 <aado97@gmail.com>
2025-02-06 15:47:18 +0900

    객체지향 연습 및 응용

--- lecture-ex/SwiftDay07LectureOOP/SwiftDay07LectureOOP/practice-shape/ShapeView.swift
+++ lecture-ex/SwiftDay07LectureOOP/SwiftDay07LectureOOP/practice-shape/ShapeView.swift
@@ -16,7 +16,13 @@ class ShapeView {
     // 선언과 동시에 초기화
     // let shapeList = [Circle(), Rectangle(), Circle(), Circle()]
     // 빈 배열에 데이터 추가: append() 사용.
-    var shapeList: [Shape] = []
+    // let으로 선언하면 배열을 변경하거나 요소를 바꿀 수 없다.
+    var shapeList: [Shape]
+    
+    init() {
+        // init()함수의 목적은 초기화
+        shapeList = [Circle(), Rectangle()]
+    }
     
     func showList() {
         for shape in shapeList {
@@ -24,6 +30,23 @@ class ShapeView {
         }
     }
     
+    func inputShape() {
+        print("[1] Circle [2] Rectangel")
+        guard let choice = readLine(), let intChoice = Int(choice) else {
+            print("잘못 입력하셨습니다.")
+            return
+        }
+        
+        appendShape(choice: intChoice)
+    }
+    
+    func showList(shapeList: [Shape]) {
+        for shape in shapeList {
+            shape.draw()
+        }
+    }
+
+    // let을 var로 변경하면 수정 가능.
     func appendShape(choice: Int) {
         switch choice {
         case 1:
@@ -37,6 +60,20 @@ class ShapeView {
         }
     }
     
+//    func appendShape(choice: Int, newShapeList: [Shape]) -> [Shape] {
+//        // 함수형 프로그래밍 방식
+//        var shape: Shape!
+//        if choice == 1 {
+//            shape = Circle()
+//        }
+//        if choice == 2 {
+//            shape = Rectangle()
+//        }
+//        return newShapeList.count > 2 ? newShapeList + [shape] : shapeList + newShapeList + [shape]
+//    }
+    
+    
+    
     func main() {
         
         // 5회 반복 Shape 생성
@@ -45,12 +82,37 @@ class ShapeView {
         // 2번쨰 생성할 객체 타입 선택 (1)Circle (2)Rectangale : 2
         // 리스트에 Rectangle 객체 추가 완료 !
         
-        for i in 1...5 {
-            print("\(i)번째 생성할 객체 타입 선택 (1)Circle (2)Rectangale", terminator: ": ")
-            let choice: Int = Int(readLine() ?? "") ?? 0
-            appendShape(choice: choice)
+        //var newShapeList: [Shape] = []
+        
+//        for i in 1...5 {
+//            print("\(i)번째 생성할 객체 타입 선택 (1)Circle (2)Rectangale", terminator: ": ")
+//            let choice: Int = Int(readLine() ?? "") ?? 0
+//            appendShape(choice: choice)
+//            //newShapeList = appendShape(choice: choice, newShapeList: newShapeList)
+//        }
+        
+        // [1]입력 [2]목록 [3]종료
+        // 선택: 1
+        // [1]Circle [2]Rectangle
+        // 선택: 2
+        // Rectangle 추가 완료!
+        
+        while true {
+            print("[1]입력 [2]목록 [3]종료")
+            print("선택", terminator: ": ")
+            let no = Int(readLine() ?? "") ?? 0
+            if no == 1 {
+                inputShape()
+            }
+            if no == 2 {
+                showList()
+            }
+            if no == 3 {
+                print("종료")
+                break
+            }
         }
         
-        showList()
+        //showList(shapeList: newShapeList)
     }
 }
