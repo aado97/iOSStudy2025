ad100a60351e90a0055f7746cef2c16bbb5e196e
aado97 <aado97@gmail.com>
2025-02-06 13:09:27 +0900

    250206 실습

---
+++ lecture-ex/SwiftDay07LectureOOP/SwiftDay07LectureOOP/model/Circle.swift
@@ -0,0 +1,14 @@
+//
+//  Circle.swift
+//  SwiftDay07LectureOOP
+//
+//  Created by 도민준 on 2/6/25.
+//
+
+import Foundation
+
+class Circle: Shape {
+    override func draw() {
+        print("원을 그립니다.")
+    }
+}
